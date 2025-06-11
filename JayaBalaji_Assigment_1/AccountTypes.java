3fb95d9a3cb9d9b8126c20f1959c417ce56dc49b
JayaBalaji02 <jayabalaji057@gmail.com>
2025-06-11 12:22:14 +0530

    Delete JayaBalaji_Assigment_1 directory

--- JayaBalaji_Assigment_1/AccountTypes.java
+++
@@ -1,8 +0,0 @@
-package com.day2.assiment;
-
-public interface AccountTypes {
-	
-	void savings_acc(Customerdetails customer_details);
-	void current_acc(Customerdetails customer_details);
-	void new_acc(String name,long addharnum);
-}
