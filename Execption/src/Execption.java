a87a926033eccd723fbd781837da001fed70c727
Jaya.Balaji <jayabalaji057@gmail.com>
2025-06-12 12:01:23 +0530

    2 execption added example

--- Execption/src/Execption.java
+++ Execption/src/Execption.java
@@ -5,9 +5,19 @@ public class Execption {
         String ans[]=new String[5];
 
         try {
+            //array index out of bound execption
+
             String a[]= {"jaya","balaji","charlie","donlee,","erfan","fayaz"};
             for(int i=0;i<a.length;i++) {
                 ans[i]=a[i];
+                System.out.println(ans[i]);
+
+
+                //Null pointer execption
+                String name[]=null;
+                if(name.equals("Chennai")){
+                    System.out.println(name);
+                }
             }
         }
         catch(Exception e) {
